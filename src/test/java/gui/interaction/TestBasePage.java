d9c6948285ae852b6ad5bd8d802f1878d1b617f4
Alex <alex68md@gmail.com>
2024-09-02 17:43:01 +0300

    + Logback TestBasePage

--- src/test/java/gui/interaction/TestBasePage.java
+++ src/test/java/gui/interaction/TestBasePage.java
@@ -7,11 +7,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestBasePage {
+    private static final Logger logger = LoggerFactory.getLogger(TestBasePage.class);
     private WebDriver driver;
     private MainPage mainpage;
     String uri, user, pswd;
@@ -24,54 +27,78 @@ public class TestBasePage {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         driver = DriverFactory.init();
         mainpage = new MainPage(driver, uri);
+        logger.info("Test setup complete");
     }
     @AfterClass
     public void closeTest(){
         driver.quit();
+        logger.info("Driver closed");
     }
     @BeforeMethod
     public void initializePageObjects() {
         driver.get(uri);
+        logger.info("Navigated to URL: {}", uri);
     }
     @AfterMethod
     public void clearCookies(){
         driver.manage().deleteAllCookies();
+        logger.info("Cookies cleared");
     }
     @Test
     public void testLangButton() {
-        Assert.assertTrue(mainpage.isLanguageDisplayed(), "Language button is not visible");
+        boolean isLanguageDisplayed = mainpage.isLanguageDisplayed();
+        logger.info("Language button visibility: {}", isLanguageDisplayed);
+        Assert.assertTrue(isLanguageDisplayed, "Language button is not visible");
     }
     @Test(enabled=true)
     public void testLoginButton() {
-        Assert.assertTrue(mainpage.isLoginDisplayed(), "Login button is not visible");
+        boolean isLoginDisplayed = mainpage.isLoginDisplayed();
+        logger.info("Login button visibility: {}", isLoginDisplayed);
+        Assert.assertTrue(isLoginDisplayed, "Login button is not visible");
     }
     @Test
     public void testChangeLang() {
         String activeLang = mainpage.currentLang();
         mainpage.takeScreenshot();
+        logger.info("Current language: {}", activeLang);
+
         if (activeLang.equals("ro")) mainpage.changeLang("ru");
         else mainpage.changeLang("ro");
-        Assert.assertNotEquals(mainpage.currentLang(), activeLang, "Language is not changed");
+
+        String newLang = mainpage.currentLang();
+        logger.info("Language after change: {}", newLang);
+        Assert.assertNotEquals(newLang, activeLang, "Language is not changed");
     }
-    @Test(enabled=false)
+    @Test(enabled=false, retryAnalyzer = RetryAnalyzer.class)
     public void testAllHref() {
-        if (!mainpage.isElementVisible(mainpage.getAnchor())) Assert.fail("Anchor element is not visible, failing the test.");
+        if (!mainpage.isElementVisible(mainpage.getAnchor())) {
+            logger.error("Anchor element is not visible, failing the test.");
+            Assert.fail("Anchor element is not visible, failing the test.");
+        }
 
         List<String> failedLinks = new ArrayList<>();
         List<WebElement> tagsA = driver.findElements(By.tagName("a"));
+        mainpage.takeScreenshot();
         for (WebElement a:tagsA){
             String href = a.getAttribute("href");
             boolean httpResponse = mainpage.tryHttpRequest(href);
-            if (!httpResponse) failedLinks.add(href);
+            if (!httpResponse) {
+                failedLinks.add(href);
+                logger.warn("Failed link: {}", href);
+            }
         }
 
         if (failedLinks.isEmpty()) {
             System.out.println("All links have status: OK");
+            logger.info("All links have status: OK");
         } else {
             System.out.println("Failed links:");
+            logger.error("Failed links:");
             for (String link : failedLinks) {
                 System.out.println(link);
+                logger.error("Failed link: {}", link);
             }
+            logger.error("Some links did not return a 200 response code.");
             Assert.fail("Some links did not return a 200 response code.");
         }
     }
