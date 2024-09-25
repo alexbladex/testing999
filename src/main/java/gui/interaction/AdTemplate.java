a21bb44a24df94377c903ed321df20f443abb9cb
Alex <alex68md@gmail.com>
2024-09-25 10:15:49 +0300

    refactoring

---
+++ src/main/java/gui/interaction/AdTemplate.java
@@ -0,0 +1,20 @@
+package gui.interaction;
+import org.json.JSONObject;
+public class AdTemplate {
+    private String json = "{\n" +
+            "  \"url\": \"https://999.md/add?category=construction-and-repair&subcategory=construction-and-repair/finishing-and-facing-materials\",\n" +
+            "  \"price\": 200,\n" +
+            "  \"price_type\": \"mdl\",\n" +
+            "  \"title\": \"плитка размер 15*15 белая turkey\",\n" +
+            "  \"desc\": \"Настенная плитка кафель 15*15 цвет белая турция. 200 л/м2. Минимальный заказ 2 метра\",\n" +
+            "  \"img\": [\n" +
+            "    \"defaultAd/plitka1515.png\"\n" +
+            "  ],\n" +
+            "  \"c\": {\n" +
+            "    \"control_686\": 21099\n" +
+            "  }\n" +
+            "}";
+    public JSONObject getAd() {
+        return new JSONObject(json);
+    }
+}
--- src/main/java/gui/interaction/CabinetItemsPage.java
+++ src/main/java/gui/interaction/CabinetItemsPage.java
@@ -155,20 +155,8 @@ public class CabinetItemsPage extends LoggedInPage {
         return i;
     }
     public AdItem addDefaultAd() {
-        String json = "{\n" +
-                "  \"url\": \"https://999.md/add?category=construction-and-repair&subcategory=construction-and-repair/finishing-and-facing-materials\",\n" +
-                "  \"price\": 200,\n" +
-                "  \"price_type\": \"mdl\",\n" +
-                "  \"title\": \"плитка кафель 15*15 белая turkey\",\n" +
-                "  \"desc\": \"Настенная плитка кафель 15*15 цвет белая турция. 200 л/м2. Минимальный заказ 2 метра\",\n" +
-                "  \"img\": [\n" +
-                "    \"defaultAd/plitka1515.png\"\n" +
-                "  ],\n" +
-                "  \"c\": {\n" +
-                "    \"control_686\": 21099\n" +
-                "  }\n" +
-                "}";
-        JSONObject jsonObject = new JSONObject(json);
+        AdTemplate temp = new AdTemplate();
+        JSONObject jsonObject = new JSONObject(temp.getAd());
         AddAdPage itempage = new AddAdPage(driver);
         return itempage.submittingForm(jsonObject);
     }
